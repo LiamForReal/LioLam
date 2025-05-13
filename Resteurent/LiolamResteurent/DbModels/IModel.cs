d9a36cde264fa7b1a85543e5fb676851c021dd8d
theRealLiam <liamneir11@gmail.com>
2025-05-13 15:08:25 +0300

    updated credit card form cansel button

--- Resteurent/LiolamResteurent/DbModels/IModel.cs
+++ Resteurent/LiolamResteurent/DbModels/IModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,13 @@ namespace LiolamResteurent
     public class IModel
     {
         public string? Id { get; set; }
-     
+
+        public override bool Equals(object obj)
+        {
+            if (obj is IModel other)
+                return this.Id == other.Id;
+
+            return false;
+        }
     }
 }
